b14e0463f45ad7b42af19c94ff0132a26d5af719
rezasaddat <31197736+rezasaddat@users.noreply.github.com>
2017-10-03 15:17:47 +0700

    update api

--- api/protected/f4.php
+++ api/protected/f4.php
@@ -9,8 +9,8 @@
 
 		public function requestData($target){
 			switch($target){
-				case "f40" : $resultList = $this->getAllLingkupArea(); break;
-				case "f401": $resultList = $this->getTimWilayah(); break;
+				case "f40" : $resultList = $this->getTimWilayah(); break;
+				case "f401": $resultList = $this->getAllLingkupArea(); break;
 				case "f431": $resultList = $this->getBentukLembaga(); break;
 				default	   : $resultList = array( "feedStatus" => "failed", "feedType" => "danger", "feedMessage" => "Terjadi kesalahan fatal, proses dibatalkan!", "feedData" => array()); break;
 			}
@@ -20,58 +20,102 @@
 		
 	        return $json;
 		}
-
+		
 		public function getAllLingkupArea(){
 	     	 /* initial condition */
+	     	$resultList			= array();
 			$error				= 0;
 			$errorType  		= "";
 			$errorMsg			= "";
-			$dumb 				= ""; 
-			$data['provinsi'] 	= ""; 
-			$data['wilayah']  	= ""; 
-			$data['kecamatan']	= ""; 
-			$data['kelurahan']	= ""; 
+			$dumb 				= "";
 		
 			/* open connection */ 
 			$gate = $this->db;
 			if($gate){		
 				// connection = true
-				$sql  = "SELECT * from (select 'f411' as `group`, a.idData, kodeProvinsi as noreg, namaProvinsi as caption, '' as `referencesKey`, '' as `references` FROM `dplega_100_provinsi` a LEFT JOIN  `dplega_101_wilayah` b ON a.idData = b.idProvinsi LEFT JOIN  `dplega_102_kecamatan` c ON b.idData = c.idWilayah order by a.idData) as table_1";
-		        $dumb = $this->db->query($sql); 
-		        $data['provinsi'] = $dumb->fetchAll(PDO::FETCH_OBJ);
+				$sql  = "SELECT 
+							kl.idData as idKelurahan, 
+							kl.kodeKelurahan, 
+							namaKelurahan, 
+							kc.idData as idKecamatan,
+							kc.kodeKecamatan,
+							namaKecamatan, 
+							wl.idData as idWilayah,
+							wl.kodeWilayah,
+							namaWilayah, 
+							pr.idData as idProvinsi,
+							pr.kodeProvinsi,
+							namaProvinsi
+						FROM
+							dplega_100_provinsi pr
+						JOIN
+							dplega_101_wilayah wl
+							ON pr.idData = wl.idProvinsi
+						JOIN
+							dplega_102_kecamatan kc
+							ON wl.idData = kc.idWilayah
+						JOIN
+							dplega_103_kelurahan kl
+							ON kc.idData = kl.idKecamatan
+						ORDER BY namaKelurahan ASC";
 
-		        $sql = 	"SELECT * from (SELECT 'f412' as `group`, a.idData, a.kodeWilayah as noreg, a.namaWilayah as caption, a.idProvinsi as `referencesKey`, namaProvinsi as `references` FROM `dplega_101_wilayah` a LEFT JOIN  `dplega_100_provinsi` b ON a.idProvinsi = b.idData LEFT JOIN  `dplega_102_kecamatan` c ON c.idWilayah = a.idData order by a.idData) as table_2";
-		        $dumb = $this->db->query($sql); 
-		        $data['wilayah'] = $dumb->fetchAll(PDO::FETCH_OBJ);
+				
+		        $dumb = $this->db->query($sql);
+		        if($dumb){
+				$data = $dumb->fetchAll();
+					// output data of each row
+					$fetchTemp		= "";
+					$fetch 	  		= array();
+					$fetchDetail	= array();
+					$record    		= array();
+					$recordDetail   = array();
+					foreach($data as $row) {
+						
+						unset($fetch); $fetch = array();
+						
+						$fetch 		   = $row['namaKelurahan'].", ".$row['namaKecamatan']." ".$row['namaWilayah']." | ".$row['namaProvinsi'];
+						$fetchDetail = array(
+									"idKelurahan" 	=> $row['idKelurahan'],
+									"kodeKelurahan" => $row['kodeKelurahan'],
+									"namaKelurahan" => $row['namaKelurahan'],
+									"idKecamatan" 	=> $row['idKecamatan'],
+									"kodeKecamatan" => $row['kodeKecamatan'],
+									"namaKecamatan" => $row['namaKecamatan'],
+									"idWilayah" 	=> $row['idWilayah'],
+									"kodeWilayah" 	=> $row['kodeWilayah'],
+									"namaWilayah" 	=> $row['namaWilayah'],
+									"idProvinsi" 	=> $row['idProvinsi'],
+									"kodeProvinsi" 	=> $row['kodeProvinsi'],
+									"namaProvinsi" 	=> $row['namaProvinsi']
+								);
+						
+						array_push($record, $fetch); 
+						array_push($recordDetail, $fetchDetail); 
 
-		        $sql = 	"SELECT * from (SELECT 'f413' as `group`, a.idData, a.kodeKecamatan as noreg, a.namaKecamatan as caption, a.idWilayah as `referencesKey`, namaWilayah as `references` FROM `dplega_102_kecamatan` a LEFT JOIN  `dplega_101_wilayah` b ON a.idWilayah = b.idData LEFT JOIN  `dplega_100_provinsi` c ON b.idProvinsi = c.idData order by a.idData) as table_3";
-		        $dumb = $this->db->query($sql); 
-		        $data['kecamatan'] = $dumb->fetchAll(PDO::FETCH_OBJ);
-
-		        $sql = 	"SELECT * from (SELECT 'f414' as `group`, a.idData, a.kodeKelurahan as noreg, a.namaKelurahan as caption, a.idKecamatan as `referencesKey`, namaKecamatan as `references` FROM `dplega_103_kelurahan` a LEFT JOIN  `dplega_102_kecamatan` b ON a.idKecamatan = b.idData  LEFT JOIN `dplega_101_wilayah` c ON b.idWilayah = c.idData  LEFT JOIN  `dplega_100_provinsi` d ON c.idProvinsi = d.idData order by a.idData) as table_4";
-		        $dumb = $this->db->query($sql); 
-		        $data['kelurahan'] = $dumb->fetchAll(PDO::FETCH_OBJ); 
-
-		        $resultList = array( "feedStatus" => "succes", "feedMessage" => "Data ditemukan!", "feedData" => array($data));
-				$gate = null;
+					}
+					
+					$resultList = array( "feedStatus" => "succes", "feedMessage" => "Data ditemukan!", "feedData" => array($record), "feedDataDetail" =>  array("list" => $recordDetail));
+				}else {
+					$resultList = array( "feedStatus" => "succes", "feedMessage" => "Data tidak ditemukan!", "feedData" => array());
+				}
 			}else {
-				//error state
-				$error		= 1;
-				$errorType  = "danger";
-				$errorMsg	= "Terjadi kesalahan, tidak dapat terhubung ke server!";
-			}
-			
-			
-			if($error == 1){
-				//error state
-				$resultList = array( "feedStatus" => "failed", "feedType" => $errorType, "feedMessage" => $errorMsg);
-			}
-			
-			/* result fetch */
-			$json = $resultList;
-		
-	        return $json;
+			//error state
+			$error		= 1;
+			$errorType  = "danger";
+			$errorMsg	= "Terjadi kesalahan, tidak dapat terhubung ke server!";
 		}
+		
+		
+		if($error == 1){
+			//error state
+			$resultList = array( "feedStatus" => "failed", "feedType" => $errorType, "feedMessage" => $errorMsg);
+		}
+		
+		/* result fetch */
+		$json = $resultList;
+		
+		return $json;
+	}
 
 		public function getTimWilayah(){
 	     	 /* initial condition */
@@ -85,30 +129,119 @@
 			$gate = $this->db;
 			if($gate){		
 				// connection = true
-				$sql  = "SELECT idData, namaTim FROM dplega_110_timwilayah ORDER BY namaTim ASC";
+				$sql = 	"	SELECT 
+							kl.idData as idKelurahan, 
+							kl.kodeKelurahan, 
+							namaKelurahan, 
+							kc.idData as idKecamatan,
+							kc.kodeKecamatan,
+							namaKecamatan, 
+							wl.idData as idWilayah,
+							wl.kodeWilayah,
+							namaWilayah, 
+							pr.idData as idProvinsi,
+							pr.kodeProvinsi,
+							namaProvinsi
+						FROM
+							dplega_100_provinsi pr
+						JOIN
+							dplega_101_wilayah wl
+							ON pr.idData = wl.idProvinsi
+						JOIN
+							dplega_102_kecamatan kc
+							ON wl.idData = kc.idWilayah
+						JOIN
+							dplega_103_kelurahan kl
+							ON kc.idData = kl.idKecamatan
+						ORDER BY namaKelurahan ASC
+					";
 		        $dumb = $this->db->query($sql); 
-		        $data = $dumb->fetchAll(PDO::FETCH_OBJ);
+		        
+		        if($dumb){
+					$data = $dumb->fetchAll();
+					// output data of each row
+					$fetchProvinsi	= array();
+					$fetchWilayah	= array();
+					$fetchKecamatan	= array();
+					$fetchKelurahan	= array();
+					$fetchDProvinsi	= array();
+					$fetchDWilayah	= array();
+					$fetchDKecamatan= array();
+					$fetchDKelurahan= array();
 
-		        $resultList = array( "feedStatus" => "succes", "feedMessage" => "Data ditemukan!", "feedData" => $data);
-				$gate = null;
+					$record    		= array();
+					$recordDetail   = array();
+					foreach($data as $row) {
+						array_push($fetchKelurahan  , $row['namaKelurahan'].", ".$row['namaKecamatan']." ".$row['namaWilayah']." | ".$row['namaProvinsi']);
+						array_push($fetchKecamatan  , $row['namaKecamatan']." ".$row['namaWilayah']." | ".$row['namaProvinsi']);
+						array_push($fetchWilayah    , $row['namaWilayah']." | ".$row['namaProvinsi']);
+						array_push($fetchProvinsi   , $row['namaProvinsi']);
+
+						array_push($fetchDKelurahan, array( 
+							"group" => "kelurahan",
+							"idData"=> $row['idKelurahan'],
+							"kode" 	=> $row['kodeKelurahan'],
+							"nama" 	=> $row['namaKelurahan']
+						));
+
+						array_push($fetchDKecamatan, array(
+							"group" => "kecamatan",
+							"idData"=> $row['idKecamatan'],
+							"kode" 	=> $row['kodeKecamatan'],
+							"nama" 	=> $row['namaKecamatan']
+						));
+
+						array_push($fetchDWilayah, array(
+							"group" => "wilayah",
+							"idData"=> $row['idWilayah'],
+							"kode" 	=> $row['kodeWilayah'],
+							"nama" 	=> $row['namaWilayah']
+						));
+
+						array_push($fetchDProvinsi, array(
+							"group" => "provinsi",
+							"idData"=> $row['idProvinsi'],
+							"kode" 	=> $row['kodeProvinsi'],
+							"nama" 	=> $row['namaProvinsi']
+						));
+					}
+
+					$record = array(
+						"kelurahan" => $fetchKelurahan,
+						"kecamatan" => $fetchKecamatan,
+						"wilayah"	=> $fetchWilayah,
+						"provinsi"	=> $fetchProvinsi
+					);
+
+					$recordDetail = array(
+						"kelurahan" => $fetchDKelurahan,
+						"kecamatan" => $fetchDKecamatan,
+						"wilayah"	=> $fetchDWilayah,
+						"provinsi"	=> $fetchDProvinsi
+					);
+					
+					$resultList = array( "feedStatus" => "succes", "feedMessage" => "Data ditemukan!", "feedData" => $record, "feedDataDetail" => $recordDetail);
+				}else {
+					$resultList = array( "feedStatus" => "succes", "feedMessage" => "Data tidak ditemukan!", "feedData" => array());
+				}
 			}else {
-				//error state
-				$error		= 1;
-				$errorType  = "danger";
-				$errorMsg	= "Terjadi kesalahan, tidak dapat terhubung ke server!";
-			}
-			
-			
-			if($error == 1){
-				//error state
-				$resultList = array( "feedStatus" => "failed", "feedType" => $errorType, "feedMessage" => $errorMsg);
-			}
-			
-			/* result fetch */
-			$json = $resultList;
-		
-	        return $json;
+			//error state
+			$error		= 1;
+			$errorType  = "danger";
+			$errorMsg	= "Terjadi kesalahan, tidak dapat terhubung ke server!";
 		}
+		
+		
+		if($error == 1){
+			//error state
+			$resultList = array( "feedStatus" => "failed", "feedType" => $errorType, "feedMessage" => $errorMsg);
+		}
+		
+		/* result fetch */
+		$json = $resultList;
+		
+		return $json;
+	}
 
 		public function getBentukLembaga(){
 	     	 /* initial condition */
